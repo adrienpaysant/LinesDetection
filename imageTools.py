634612c9c7dfc8b34b38f97c5c99f26a3ac062ac
Paysant Adrien <adrien.paysant@he-arc.ch>
2021-03-09 10:19:50 +0100

    edge detect function fixed + rename images files

--- imageTools.py
+++ imageTools.py
@@ -5,9 +5,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 def edgeDetectAndShow(imagePath):
-    img = cv2.imread(imagePath,0)
+    img = cv2.imread(imagePath)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(img,100,200)
-    plt.subplot(121),plt.imshow(img,cmap = 'gray')
+    plt.subplot(121),plt.imshow(img,cmap = 'hsv')
     plt.title('Original Image'), plt.xticks([]), plt.yticks([])
     plt.subplot(122),plt.imshow(edges,cmap = 'gray')
     plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import imageTools
+import ImageTools 
 import os
 import cv2
 
@@ -6,8 +6,10 @@ import cv2
 if __name__ == "__main__":
     print('Main Launching')
 
+
     path='./source_images/'
-    
-    
+    file="0.jpg"
+    ImageTools.edgeDetectAndShow(os.path.join(path, file))
+
 
     cv2.waitKey(0)
