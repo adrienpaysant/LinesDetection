2852c508b24a2ae1d7b747dd94ed0e032601b47d
Monnet Joris <joris.monnet@he-arc.ch>
2021-05-04 01:34:51 +0200

    change the sort value and manage case where balls are not on the line

--- imageTools.py
+++ imageTools.py
@@ -4,10 +4,12 @@ import numpy as np
 import numpy as np
 from matplotlib import pyplot as plt
 from win32api import GetSystemMetrics
+from shapely.geometry import LineString
+from shapely.geometry import Point
 import math
 
-WIDTH_SCREEN= GetSystemMetrics(0)
-HEIGHT_SCREEN= GetSystemMetrics(1)
+WIDTH_SCREEN = GetSystemMetrics(0)
+HEIGHT_SCREEN = GetSystemMetrics(1)
 
 ############################################################
 ############################################################
@@ -35,15 +37,44 @@ class Marble:
     def __str__(self):
         return f"center = {self.center} and radius = {self.radius}"    
 
+# from https://stackoverflow.com/questions/30844482/what-is-most-efficient-way-to-find-the-intersection-of-a-line-and-a-circle-in-py
+def getIntersectionLineCircle(center,radius,x1,y1,x2,y2):
+    p = Point(center[0],center[1])
+    c = p.buffer(radius).boundary
+    l = LineString([(x1,y1), (x2, y2)])
+    return  c.intersection(l)
+
 #https://stackoverflow.com/questions/47177493/python-point-on-a-line-closest-to-third-point
-def getClosestPointOnLine(p1,p2,p3):
-    x1, y1 = p1
-    x2, y2 = p2
-    x3, y3 = p3
+def getClosestPointOnLine(marble1,marble2,marbleRunning):
+    x1, y1 = marble1.center
+    x2, y2 = marble2.center
+    x3, y3 = marbleRunning.center
     dx, dy = x2-x1, y2-y1
     coef = dx*dx + dy*dy
     a = (dy*(y3-y1)+dx*(x3-x1))/coef
-    return int(x1+a*dx), int(y1+a*dy)
+    x = int(x1+a*dx)
+    y = int(y1+a*dy)
+    
+    marble1Intersection = getIntersectionLineCircle(marble1.center,marble1.radius,x1,y1,x2,y2)
+    marble2Intersection = getIntersectionLineCircle(marble2.center,marble2.radius,x1,y1,x2,y2)
+    marble1MinPoint = marble1Intersection.coords[0]
+    marble2MinPoint = marble2Intersection.coords[0]
+    x1 = int(marble1MinPoint[0])
+    x2 = int(marble2MinPoint[0])
+    y1 = int(marble1MinPoint[1])
+    y2 = int(marble2MinPoint[1])
+
+    xMin = min(x1,x2)
+    xMax = max(x1,x2)
+    yMin = min(y1,y2)
+    yMax = max(y1,y2)
+    if(xMin<=x and yMin<=y):
+        if(x<=xMax and y <=yMax):
+            return x, y
+        else :
+            return xMax,yMax
+    else :
+        return xMin,yMin
 
 def getSideOfLine(p1,p2,p3):
     x1, y1 = p1
@@ -54,24 +85,24 @@ def getSideOfLine(p1,p2,p3):
     print ('A marble Won')
     return True
 
-def getMask(grayImage,lowerValueRange):
-    mask = cv2.GaussianBlur(grayImage, (11, 11), 0)
+def getMask(grayImage,lowerValueRange,ksize):
+    mask = cv2.GaussianBlur(grayImage, (ksize, ksize), 0)
     mask = cv2.inRange(mask, lowerValueRange, 255)
     return mask
 
-def erodeDilate(grayImage,mask):
+def erodeDilate(grayImage,mask, erodeIterations,dilateIterations):
     res = cv2.bitwise_and(grayImage, grayImage, mask=mask)
-    res = cv2.erode(res, None, iterations=3)
-    res = cv2.dilate(res, None, iterations=1)
+    res = cv2.erode(res, None, iterations=erodeIterations)
+    res = cv2.dilate(res, None, iterations=dilateIterations)
     return res
 
-def getX(elem):
-    return elem.center[0]
+def getY(elem):
+    return elem.center[1]
 
-def shapeDetection(img,lowerValueRange,radiusArrivalObject):
+def shapeDetection(img,lowerValueRange,radiusArrivalObject,radiusMin,erodeIterations,dilateIterations,ksize):
     grayImage = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    mask = getMask(grayImage,lowerValueRange)
-    result = erodeDilate(grayImage,mask)
+    mask = getMask(grayImage,lowerValueRange,ksize)
+    result = erodeDilate(grayImage,mask,erodeIterations,dilateIterations)
 
     newImage = img
     contours, hierarchy = cv2.findContours(result, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -81,19 +112,19 @@ def shapeDetection(img,lowerValueRange,radiusArrivalObject):
     for cnt in contours:
         M = cv2.moments(cnt)
         (x,y),radius = cv2.minEnclosingCircle(cnt)
-        if(radius > 50 and radius <300):
+        if(radius > radiusMin and radius <300):
             center = (int(x),int(y))
             radius = int(radius)
             marblesSet.add(Marble(center,radius))
             cv2.circle(newImage,center,radius,(0,255,0),4)
-    #showAndWait("marbles contours",newImage)
+    # showAndWait("marbles contours",newImage)
     # print([str(marble) for marble in marblesSet])
     arrivalLineMarbles = []
     for marble in marblesSet:
         if marble.radius>radiusArrivalObject:
             arrivalLineMarbles.append(marble)
 
-    arrivalLineMarbles.sort(key=getX)
+    arrivalLineMarbles.sort(key=getY,reverse=True)
     cv2.line(newImage,arrivalLineMarbles[0].center,arrivalLineMarbles[1].center,(255,0,0),5)
     runningMarbles = set([marble for marble in marblesSet.difference(set(arrivalLineMarbles))])
     arrivalCenter1=arrivalLineMarbles[0].center
@@ -101,7 +132,7 @@ def shapeDetection(img,lowerValueRange,radiusArrivalObject):
     # print([str(marble) for marble in runningMarbles])
     distances = []
     for marble in runningMarbles : 
-        linePoint = getClosestPointOnLine(arrivalCenter1,arrivalCenter2,marble.center)
+        linePoint = getClosestPointOnLine(arrivalLineMarbles[0],arrivalLineMarbles[1],marble)
         if(getSideOfLine(arrivalCenter1,arrivalCenter2,marble.center)):
             cv2.line(newImage,marble.center,linePoint,(0,255,255),3)
         else :
@@ -114,12 +145,15 @@ def shapeDetection(img,lowerValueRange,radiusArrivalObject):
     return newImage
 
 def shapeDetectionOnVideo(img):
-    """ Return an image with shape detected """    
-    return shapeDetection(img,100,80)
+    return shapeDetection(img,100,80,50,3,1,11)
 
 def shapeDetectionOnImage(imagePath):
     img = cv2.imread(imagePath)
-    return shapeDetection(img,127,150)
+    return shapeDetection(img,90,100,30,0,7,7)
+
+def shapeDetectionOnOldImage(imagePath):
+    img = cv2.imread(imagePath)
+    return shapeDetection(img,127,150,50,3,1,11)
 ############################################################
 ############################################################
 ##############Caracteristiques & Display####################
--- main.py
+++ main.py
@@ -4,17 +4,7 @@ import cv2 as cv
 import numpy
 import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
-    print('Main Launching')
-
-
-    path='./source_images/'
-    file="0.jpg"
-    imgPath=os.path.join(path, file)
-
-    for i in range(2):
-        imageTools.showAndWait("basic shape detect",imageTools.shapeDetectionOnImage(f"./source_images/{i}.jpg"))
-
+def videoProgram():
     video = cv.VideoCapture("./source_videos/data.mp4")
     newVideo = cv.VideoWriter("newVideo.avi",cv.CAP_OPENCV_MJPEG,cv.VideoWriter_fourcc('M','J','P','G'),10,(1920,1080),True)
     numberOfFrame = 0
@@ -31,5 +21,17 @@ if __name__ == "__main__":
     # Release all space and windows once done
     video.release()
     newVideo.release()
+
+if __name__ == "__main__":
+    print('Main Launching')
+
+
+    imageTools.showAndWait("basic shape detect",imageTools.shapeDetectionOnOldImage(f"./source_images/{0}.jpg"))
+    imageTools.showAndWait("basic shape detect",imageTools.shapeDetectionOnOldImage(f"./source_images/{1}.jpg"))
+    for i in range(2,6):
+        imageTools.showAndWait("basic shape detect",imageTools.shapeDetectionOnImage(f"./source_images/{i}.jpg"))
+
+    # videoProgram()
     cv.waitKey(0)
     print('Main End')
+
